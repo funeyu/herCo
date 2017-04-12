731d9df88540301e866ade40e3a2af44745047f1
funeyu <funeyu@gmail.com>
2017-04-13 00:00:13 +0800

    test code

--- test.js
+++ test.js
@@ -14,52 +14,52 @@ herCo(function *(){
   console.log(error)
 });
 
-// // ===========================================================
-// herCo(function*(){
-//     var data1 = yield readFile('herCo.js');
-//     console.log(data1);
-//     var data2 = yield readFile('herCo.js');
-//     console.log(data2);
-// });
-//
-// function readFile( path ){
-//     return function(callback){
-//        fs.readFile( path , 'utf-8', callback);
-//     }
-// }
-//
-// // ============================================================
-// herCo(function* () {
-//   try {
-//     var result = yield Promise.reject("true");
-//     var result2 = yield new Promise(function(resolve, reject) {
-//       setTimeout(function(){
-//         resolve('finished here')
-//       }, 1000)
-//     })
-//     return result2;
-//   } catch(error) {
-//     return 'success'
-//   }
-//
-// }).then(function (value) {
-//   console.log(value);
-// }, function (err) {
-//   console.error('warning:' + err);
-// });
-//
-// // ===============================================================
-// herCo(function* () {
-//     var result = yield Promise.reject("true");
-//     var result2 = yield new Promise(function(resolve, reject) {
-//       setTimeout(function(){
-//         resolve('finished here')
-//       }, 1000)
-//     })
-//     return result2;
-//
-// }).then(function (value) {
-//   console.log(value);
-// }, function (err) {
-//   console.error('warning:' + err);
-// });
+// ===========================================================
+herCo(function*(){
+    var data1 = yield readFile('herCo.js');
+    console.log(data1);
+    var data2 = yield readFile('herCo.js');
+    console.log(data2);
+});
+
+function readFile( path ){
+    return function(callback){
+       fs.readFile( path , 'utf-8', callback);
+    }
+}
+
+// ============================================================
+herCo(function* () {
+  try {
+    var result = yield Promise.reject("true");
+    var result2 = yield new Promise(function(resolve, reject) {
+      setTimeout(function(){
+        resolve('finished here')
+      }, 1000)
+    })
+    return result2;
+  } catch(error) {
+    return 'success'
+  }
+
+}).then(function (value) {
+  console.log(value);
+}, function (err) {
+  console.error('warning:' + err);
+});
+
+// ===============================================================
+herCo(function* () {
+    var result = yield Promise.reject("true");
+    var result2 = yield new Promise(function(resolve, reject) {
+      setTimeout(function(){
+        resolve('finished here')
+      }, 1000)
+    })
+    return result2;
+
+}).then(function (value) {
+  console.log(value);
+}, function (err) {
+  console.error('warning:' + err);
+});
